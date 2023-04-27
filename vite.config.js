14b5e1d49cb834973325b06bee3a5c52710f80eb
Asano-Naoki <asannaoki@gmail.com>
2023-04-27 16:51:35 +0900

    SPAの404対策build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import { resolve } from "path";
 import { defineConfig } from 'vite'
+
 import vue from '@vitejs/plugin-vue'
 import dsv from '@rollup/plugin-dsv'
 
@@ -9,7 +11,13 @@ export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/shihoushiken/' : './',
   plugins: [vue(), dsv()],
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, '404.html'),
+      }
+    }    
   },
   resolve: {
     alias: {

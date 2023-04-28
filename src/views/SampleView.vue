4d398180bb6849ec32d0883036bc63089de9b7f8
Asano-Naoki <asannaoki@gmail.com>
2023-04-29 08:18:14 +0900

    サンプルページの選択肢と答え

--- src/views/SampleView.vue
+++ src/views/SampleView.vue
@@ -1,11 +1,27 @@
 <template>
   <main>
-    <div style="border:solid; height:1000px;">
+    <div style="height:1000px;">
       <h1>サンプルページ</h1>
       <div>
+        <!--問題部分-->
         <v-card title="2020年民法第11問" variant="outlined">
-          <v-card-text class="q">{{ text.replace(/\n/g,'\n\n') }}</v-card-text>
+          <v-card-text class="q">{{ filteredQ }}</v-card-text>
         </v-card>
+
+        <!--選択肢部分-->
+        <v-table>
+          <tbody>
+            <tr
+              v-for="choice in filteredChoices"
+            >
+              <td><v-btn @click="getResult(choice)">{{ choice.substr(0, 2) }}</v-btn></td>
+              <td>{{ choice.substr(2) }}</td>
+            </tr>
+          </tbody>
+        </v-table>
+
+        <!--正誤結果部分-->
+        <p>{{ result }}</p>
       </div>
     </div>
   </main>
@@ -17,13 +33,36 @@ import result from "../data/min2020.csv";
 export default {
   data() {
     return {
-      text: result[10].q,
-      result: {},
+      q: result[10].q,
+      choices: result[10],
+      a: result[10].a,
+      result: '',
     }
   },
   created() {
     console.log(result[10].q)
   },
+  computed: {
+    filteredQ() {
+      return this.q.replace(/\n/g,'\n\n')
+    },
+    filteredChoices() {
+      delete this.choices.q
+      delete this.choices.a
+      return this.choices
+    }
+  },
+  methods: {
+    hankaku2Zenkaku(str) {
+      return str.replace(/[０-９]/g, function(s) {
+        return String.fromCharCode(s.charCodeAt(0) - 0xFEE0)
+      })
+    },
+    getResult(choice) {
+      const selectedChoice = choice.substr(0,1)
+      this.result = this.hankaku2Zenkaku(selectedChoice) == this.a ? '正解' : '不正解'
+    }
+  }
 }
 </script>
 
